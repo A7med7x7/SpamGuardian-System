1f9f0a2f74f49f8016ce3e888feefb33aac8ad4f
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2024-07-30 04:31:33 +0000

    the classifer API works fine now

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route("/", methods=["GET", "POST"])
-def home():
-    text =""
-    if request.method == "POST":
-        text = request.form.get('email-content')
-    return render_template('index.html',text=text)
+import pickle
 
+tokenizer = pickle.load(open('models/cv.pkl', 'rb'))
+model = pickle.load(open('models/clf.pkl', 'rb'))
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    #if request.method == 'POST':
+    email = request.form.get("content")
+    tokenized_email = tokenizer.transform([email])
+    predictions = model.predict(tokenized_email)
+    predictions = 1 if predictions == 1 else -1
+    return render_template('index.html', predictions=predictions, email_text=email)
 if __name__ == "__main__": 
     app.run(debug=True)
